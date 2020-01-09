6b65bdab2a013112023c76ebe1138dd92ef03154
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-01-09 08:32:58 -0700

    selectors

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p");
+const author = document.querySelector("#author");
+const resetButton = document.querySelector("#reset");
+const nextButton = document.querySelector("#next");
+const record = document.querySelector("#record p");
+const theTimer = document.querySelector(".timer");
+
